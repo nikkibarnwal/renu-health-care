ee069e8eb5456b6b5dbe1b41e948973e8fd7a903
Nikki Burnwal <nikkibarnwal44@gmail.com>
2024-10-09 16:55:02 +0530

    resolved multiple issue

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
 
 const About = () => {
@@ -7,7 +7,6 @@ const About = () => {
     <div className="sm:w-11/12 m-auto">
       {/* Main section */}
       <div className="bg-gray-100 overflow-hidden  ">
-
         {/* Hero Image Section */}
         <div clasName="mb-4 w-full">
           <h1 className="text-5xl md:text-4xl font-bold text-black bg-gradient-to-r from-green-600 via-teal-600 to-blue-600 bg-clip-text text-corner mb-4  ">
@@ -19,20 +18,22 @@ const About = () => {
           style={{
             backgroundImage: `url("/About-img/img1.jpg")`,
           }}
-        >
-
-        </div>
-
+        ></div>
 
         {/* Content Section */}
         <div id="2" className="lg:px-20 lg:flex my-24 sm:mx-4 overflow-hidden">
           <div className="lg:w-1/2 px-4 mt-6 lg:mt-0">
             <h3 className="text-2xl  md:text-3xl font-bold text-gray-800">
               Welcome To Welfare Organization
-              <span > Established Since 2015</span>
+              <span> Established Since 2015</span>
             </h3>
             <p className="mt-4 text-md md:text-lg text-black leading-relaxed font-semibold">
-              Our mission is to create a world where everyone has access to basic human rights, education, healthcare, and opportunities for sustainable livelihoods. We envision empowered communities working together to overcome poverty, injustice, and environmental degradation, fostering a global society built on dignity, equality, and compassion.
+              Our mission is to create a world where everyone has access to
+              basic human rights, education, healthcare, and opportunities for
+              sustainable livelihoods. We envision empowered communities working
+              together to overcome poverty, injustice, and environmental
+              degradation, fostering a global society built on dignity,
+              equality, and compassion.
             </p>
           </div>
           <div className="lg:w-1/2 px-4">
@@ -42,13 +43,10 @@ const About = () => {
               alt="About us"
             />
           </div>
-
-
         </div>
 
         {/* Content Section part 2*/}
         <div id="2" className="lg:px-20 lg:flex my-24 sm:mx-4 overflow-hidden">
-
           <div className="lg:w-1/2 px-4">
             <img
               className="w-full rounded-lg shadow-lg"
@@ -61,7 +59,12 @@ const About = () => {
               Our Vision
             </h3>
             <p className="mt-4 text-md md:text-lg text-black leading-relaxed font-semibold">
-              Our mission is to create a world where everyone has access to basic human rights, education, healthcare, and opportunities for sustainable livelihoods. We envision empowered communities working together to overcome poverty, injustice, and environmental degradation, fostering a global society built on dignity, equality, and compassion.
+              Our mission is to create a world where everyone has access to
+              basic human rights, education, healthcare, and opportunities for
+              sustainable livelihoods. We envision empowered communities working
+              together to overcome poverty, injustice, and environmental
+              degradation, fostering a global society built on dignity,
+              equality, and compassion.
             </p>
           </div>
         </div>
@@ -83,7 +86,11 @@ const About = () => {
               Join Us
             </h1>
             <p className="text-xs sm:text-sm md:text-base mb-4 leading-relaxed">
-              EVERY donation, no matter how modest, has the remarkable power to create a ripple effect of change. So, the hesitation to donate, even if a small donation, to causes we care about often with the feeling that it may not be able to make a meaningful impact is incorrect.
+              EVERY donation, no matter how modest, has the remarkable power to
+              create a ripple effect of change. So, the hesitation to donate,
+              even if a small donation, to causes we care about often with the
+              feeling that it may not be able to make a meaningful impact is
+              incorrect.
             </p>
             <button className="bg-white text-black px-3 py-2 rounded-md text-xs sm:text-sm md:text-base">
               Donate Now
@@ -91,11 +98,9 @@ const About = () => {
           </div>
         </div>
 
-
         {/* added image and form section */}
 
         <div className="flex flex-col md:flex-row m-3">
-
           {/* Image Section */}
           <div className="w-full md:w-1/2 flex justify-center">
             <img
@@ -111,7 +116,6 @@ const About = () => {
               <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl font-bold">
                 Any Suggestion for us
               </h1>
-
             </div>
             <div className="border shadow-md rounded-xl">
               <form className="flex flex-col p-3">
@@ -122,35 +126,45 @@ const About = () => {
                   placeholder="Name..."
                   required
                   onInput={(e) => {
-                    e.target.value = e.target.value.replace(/[^a-zA-Z\s]/g, '');
+                    e.target.value = e.target.value.replace(/[^a-zA-Z\s]/g, "");
                   }}
                 />
                 <label className="mb-2 font-semibold">Email</label>
-                <input className="mb-4 p-2 border border-gray-300 rounded-lg"
+                <input
+                  className="mb-4 p-2 border border-gray-300 rounded-lg"
                   required
-                  type="email" placeholder="Email..." />
+                  type="email"
+                  placeholder="Email..."
+                />
                 <label className="mb-2 font-semibold">Suggestion</label>
-                <input className="mb-4 p-2 border rounded-lg
+                <input
+                  className="mb-4 p-2 border rounded-lg
                    border-gray-300"
                   required
-                  placeholder="Your suggestion..." />
-                <button className="bg-green-700 text-white px-4 py-2 rounded-xl">Submit</button>
+                  placeholder="Your suggestion..."
+                />
+                <button className="bg-green-700 text-white px-4 py-2 rounded-xl">
+                  Submit
+                </button>
               </form>
             </div>
           </div>
-
         </div>
 
         {/* added reviews Section */}
         <div className="px-3 border-1 border-black rounded-lg pt-3 my-2 mx-3">
-          <div id="4" className="lg:px-20 text-center flex flex-col items-center mx-4 overflow-hidden">
+          <div
+            id="4"
+            className="lg:px-20 text-center flex flex-col items-center mx-4 overflow-hidden"
+          >
             <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-3xl font-bold text-gray-800 capitalize">
               LATEST DONATIONS
             </h3>
-
           </div>
-          <div id="5" className="lg:px-20 flex flex-col md:flex-row justify-evenly gap-6 my-10 mx-4 overflow-hidden">
-
+          <div
+            id="5"
+            className="lg:px-20 flex flex-col md:flex-row justify-evenly gap-6 my-10 mx-4 overflow-hidden"
+          >
             <div className="flex flex-col items-center bg-green-50 mb-8 shadow-md rounded-lg p-6 ">
               <img
                 className="w-16 h-16 rounded-full mr-4 mb-4"
@@ -159,8 +173,16 @@ const About = () => {
               />
               <div className="flex flex-col items-center text-center justify-center">
                 <p className="text-xl font-bold">Jonaki</p>
-                <p className="font-semibold">Donated <span className="text-blue-300 ">₹500</span> for food of Childrens</p>
-                <Link className="text-blue-400 pt-2 flex items-center">Donate Now <span className="text-sm"><IoIosArrowForward /></span></Link>
+                <p className="font-semibold">
+                  Donated <span className="text-blue-300 ">₹500</span> for food
+                  of Childrens
+                </p>
+                <Link className="text-blue-400 pt-2 flex items-center">
+                  Donate Now{" "}
+                  <span className="text-sm">
+                    <IoIosArrowForward />
+                  </span>
+                </Link>
               </div>
             </div>
 
@@ -172,8 +194,16 @@ const About = () => {
               />
               <div className="flex flex-col items-center text-center justify-center">
                 <p className="text-xl font-bold">Jonaki</p>
-                <p className="font-semibold">Donated <span className="text-blue-300 ">₹500</span> for food of Childrens</p>
-                <Link className="text-blue-400 pt-2 flex items-center">Donate Now <span className="text-sm"><IoIosArrowForward /></span></Link>
+                <p className="font-semibold">
+                  Donated <span className="text-blue-300 ">₹500</span> for food
+                  of Childrens
+                </p>
+                <Link className="text-blue-400 pt-2 flex items-center">
+                  Donate Now{" "}
+                  <span className="text-sm">
+                    <IoIosArrowForward />
+                  </span>
+                </Link>
               </div>
             </div>
 
@@ -185,8 +215,16 @@ const About = () => {
               />
               <div className="flex flex-col items-center text-center justify-center">
                 <p className="text-xl font-bold">Jonaki</p>
-                <p className="font-semibold">Donated <span className="text-blue-300 ">₹500</span> for food of Childrens</p>
-                <Link className="text-blue-400 pt-2 flex items-center">Donate Now <span className="text-sm"><IoIosArrowForward /></span></Link>
+                <p className="font-semibold">
+                  Donated <span className="text-blue-300 ">₹500</span> for food
+                  of Childrens
+                </p>
+                <Link className="text-blue-400 pt-2 flex items-center">
+                  Donate Now{" "}
+                  <span className="text-sm">
+                    <IoIosArrowForward />
+                  </span>
+                </Link>
               </div>
             </div>
 
@@ -194,16 +232,22 @@ const About = () => {
               <img
                 className="w-16 h-16 rounded-full mr-4 mb-4"
                 src="./About-img/7daf15f4edf74a94bb33984f8d0fd58c.png"
-
                 alt="Himanshu"
               />
               <div className="flex flex-col items-center text-center justify-center">
                 <p className="text-xl font-bold">Jonaki</p>
-                <p className="font-semibold">Donated <span className="text-blue-300 ">₹500</span> for food of Childrens</p>
-                <Link className="text-blue-400 flex items-center pt-2">Donate Now <span className="text-sm"><IoIosArrowForward /></span></Link>
+                <p className="font-semibold">
+                  Donated <span className="text-blue-300 ">₹500</span> for food
+                  of Childrens
+                </p>
+                <Link className="text-blue-400 flex items-center pt-2">
+                  Donate Now{" "}
+                  <span className="text-sm">
+                    <IoIosArrowForward />
+                  </span>
+                </Link>
               </div>
             </div>
-
           </div>
         </div>
       </div>
